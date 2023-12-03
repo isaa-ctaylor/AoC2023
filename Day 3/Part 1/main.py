9019498cc7d00015bf36b288da5f84e7c2187710
isaa-ctaylor <isaac@isaactaylor.xyz>
2023-12-03 20:58:15 +0000

    Day 3 completed

---
+++ Day 3/Part 1/main.py
@@ -0,0 +1,38 @@
+import aoc_helper as aoc
+import re
+
+d = aoc.get_input(2023, 3)
+
+symbols = []
+nums = []
+
+lines = d.splitlines()
+
+for lindex, l in enumerate(lines):
+    syms = re.finditer(r"[@/+%#*&$=-]", l)
+    for s in syms:
+        pos = (s.start(), lindex)
+        symbols.append(pos)
+    
+for s in symbols:
+    for pos in [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]:
+        if lines[(y := s[1] + pos[1])][(x := s[0] + pos[0])].isdigit():
+            while lines[y][x].isdigit():
+                x -= 1
+            nums.append((y, x+1))
+            
+total = 0
+
+for n in set(nums):
+    num = ""
+    x, y = n[0], n[1]
+    try:
+        while (d := lines[x][y]).isdigit():
+            num += d
+            y += 1
+    except IndexError:
+        pass
+        
+    total += int(num)
+    
+print(total)
---
+++ Day 3/Part 2/main.py
@@ -0,0 +1,43 @@
+import aoc_helper as aoc
+import re
+
+d = aoc.get_input(2023, 3)
+
+gears = []
+
+lines = d.splitlines()
+
+for lindex, l in enumerate(lines):
+    syms = re.finditer(r"\*", l)
+    for s in syms:
+        pos = (s.start(), lindex)
+        gears.append(pos)
+
+total = 0
+    
+for s in gears:
+    nums = []
+    for pos in [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]:
+        if lines[(y := s[1] + pos[1])][(x := s[0] + pos[0])].isdigit():
+            while lines[y][x].isdigit():
+                x -= 1
+            nums.append((y, x+1))
+            
+    if len((nums := set(nums))) == 2:
+        ratio = 1
+        for index, n in enumerate(nums):
+            num = ""
+            x, y = n[0], n[1]
+            try:
+                while (d := lines[x][y]).isdigit():
+                    num += d
+                    y += 1
+            except IndexError:
+                pass
+            
+            ratio *= int(num)
+
+        total += ratio
+
+    
+print(total)
